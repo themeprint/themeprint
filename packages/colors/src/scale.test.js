bf9548eeb421ba6a2935e38be320ddb460c1410f
devdigital <devdigital.cloud@gmail.com>
2020-04-19 15:50:44 +0100

    wip: add scale api

--- packages/colors/src/scale.test.js
+++ packages/colors/src/scale.test.js
@@ -1,7 +1,31 @@
+import { color, white, black } from './color'
 import { scale } from './scale'
 
 describe('scale', () => {
-  it('should return scale', () => {
-    expect(scale({ center }))
+  it('should return scale from range', () => {
+    expect(scale({ start: white(), end: black() }).toEqual(['', '']))
+  })
+
+  it('should return scale from center color using 80 rotation by default', () => {
+    expect(scale(white())).toEqual(['', '', ''])
   })
 })
+
+// {
+//   name: '',
+//   number: 9
+//   value: {
+//     index: number => number // takes the scale number and returns an index number, default is middle of scale
+//     generator: // object that returns previous and next functions
+//       previous: color => color,
+//       next: color => color
+//   }
+// }
+
+// have a configure which does:
+// configure(defaultOptions) => scale(valueOrRange, options)
+// and a fromScale:
+// const fromScale = (name, valueOrRange, options) => configure({ name })(valueOrRange, options)
+
+// 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4
