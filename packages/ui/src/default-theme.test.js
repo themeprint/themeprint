4179d6f638450215d33d569dc0d00c1a8eef5e6f
devdigital <devdigital.cloud@gmail.com>
2020-05-03 15:31:04 +0100

    feat: export default theme

--- packages/ui/src/default-theme.test.js
+++ packages/ui/src/default-theme.test.js
@@ -11,14 +11,22 @@ describe('default theme', () => {
     'highlight',
   ]
 
-  each(expectedColors.map(c => [c])).test(`contains color '%s'`, color => {
+  each(expectedColors.map(c => [c])).it(`contains color '%s'`, color => {
     expect(defaultTheme.colors[color]).toBeTruthy()
   })
 
-  each(expectedColors.map(c => [c])).test(
+  each(expectedColors.map(c => [c])).it(
     `contains dark mode color '%s'`,
     color => {
       expect(defaultTheme.modes.dark[color]).toBeTruthy()
     }
   )
+
+  it('should contain space scale', () => {
+    expect(defaultTheme.space.length).toEqual(9)
+  })
+
+  it('should contain font scale', () => {
+    expect(defaultTheme.fontSizes.length).toEqual(9)
+  })
 })
