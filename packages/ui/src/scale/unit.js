296dd92f8fbc370018939f96c58d1b54b54ebffd
devdigital <devdigital.cloud@gmail.com>
2020-05-12 16:19:43 +0100

    wip: add unit tests

--- packages/ui/src/scale/unit.js
+++ packages/ui/src/scale/unit.js
@@ -1,20 +1,102 @@
 import { isNumeric, isString, isNil } from '@utilz/types'
 
+const toNumeric = value => {
+  const invalid = {
+    isValid: false,
+    value: undefined,
+  }
+
+  if (isNil(value)) {
+    return invalid
+  }
+
+  if (!isNumeric(value)) {
+    return invalid
+  }
+
+  const result = Number(value)
+
+  if (isNaN(result)) {
+    return invalid
+  }
+
+  return {
+    isValid: true,
+    value: result,
+  }
+}
+
+const isValidUnit = value => {
+  if (!value) {
+    return false
+  }
+
+  if (!isString(value)) {
+    return false
+  }
+
+  const validUnits = [
+    'cm',
+    'mm',
+    'in',
+    'px',
+    'pt',
+    'pc',
+    'em',
+    'ex',
+    'ch',
+    'rem',
+    'vw',
+    'vh',
+    'vmin',
+    'vmax',
+    '%',
+  ]
+
+  return validUnits.includes(value)
+}
+
 export const unit = value => {
   if (isNil(value)) {
     throw new Error('No value specified.')
   }
 
-  if (isNumeric(value)) {
+  const invalid = () => {
+    throw new Error(
+      `The value '${
+        isString(value) ? value : JSON.stringify(value)
+      }' is not a valid unit.`
+    )
+  }
+
+  const number = toNumeric(value)
+  if (number.isValid) {
     return {
-      value: value,
+      value: number.value,
       unit: 'px',
     }
   }
 
   if (isString(value)) {
-    return ''
+    const numParts = value.split(/([\d|.]+)/).filter(Boolean)
+    if (!numParts || numParts.length !== 2) {
+      invalid()
+    }
+
+    const number = toNumeric(numParts[0])
+    if (!number.isValid) {
+      invalid()
+    }
+
+    if (!isValidUnit(numParts[1])) {
+      invalid()
+    }
+
+    return {
+      value: number.value,
+      unit: numParts[1],
+    }
   }
 
-  throw new Error('Expected string or numeric value.')
+  invalid()
 }
--- packages/ui/src/scale/unit.test.js
+++ packages/ui/src/scale/unit.test.js
@@ -10,21 +10,117 @@ describe('unit', () => {
   })
 
   it('should throw with boolean value', () => {
-    expect(() => unit(false)).toThrow('Expected string or numeric value.')
+    expect(() => unit(false)).toThrow(`The value 'false' is not a valid unit.`)
   })
 
   it('should throw with object value', () => {
-    expect(() => unit({})).toThrow('Expected string or numeric value.')
+    expect(() => unit({})).toThrow(`The value '{}' is not a valid unit.`)
   })
 
   it('should throw with array value', () => {
-    expect(() => unit([])).toThrow('Expected string or numeric value.')
+    expect(() => unit([])).toThrow(`The value '[]' is not a valid unit.`)
   })
 
   it('should return px unit with integer value', () => {
-    expect(unit(1)).toEqual({
+    expect(unit(0)).toEqual({
       unit: 'px',
-      value: 1,
+      value: 0,
     })
+  })
+
+  it('should return px unit for numeric value', () => {
+    expect(unit(1.25)).toEqual({
+      unit: 'px',
+      value: 1.25,
+    })
+  })
+
+  it('should return px unit for integer with no postfix', () => {
+    expect(unit('0')).toEqual({
+      unit: 'px',
+      value: 0,
+    })
+  })
+
+  it('should return px unit for numeric value with no postfix', () => {
+    expect(unit('0.22')).toEqual({
+      unit: 'px',
+      value: 0.22,
+    })
+  })
+
+  it('should return px unit for numeric value with no postfix', () => {
+    expect(unit('0.22')).toEqual({
+      unit: 'px',
+      value: 0.22,
+    })
+  })
+
+  it('should return px unit for integer with px', () => {
+    expect(unit('22px')).toEqual({
+      unit: 'px',
+      value: 22,
+    })
+  })
+
+  it('should return px unit for numeric with px', () => {
+    expect(unit('22.42px')).toEqual({
+      unit: 'px',
+      value: 22.42,
+    })
+  })
+
+  it('should return % unit for string with %', () => {
+    expect(unit('22%')).toEqual({
+      unit: '%',
+      value: 22,
+    })
+  })
+
+  it('should return ch unit for string with ch', () => {
+    expect(unit('22ch')).toEqual({
+      unit: 'ch',
+      value: 22,
+    })
+  })
+
+  it('should throw for invalid numeric value', () => {
+    expect(() => unit('22..22ch')).toThrow(
+      `The value '22..22ch' is not a valid unit.`
+    )
+  })
+
+  it('should throw for invalid unit', () => {
+    expect(() => unit('22pf')).toThrow(`The value '22pf' is not a valid unit.`)
+  })
+
+  it('should throw for not leading numeric value', () => {
+    expect(() => unit('foo22')).toThrow(
+      `The value 'foo22' is not a valid unit.`
+    )
+  })
+
+  it('should throw for unexpected postfix', () => {
+    expect(() => unit('22px foo')).toThrow(
+      `The value '22px foo' is not a valid unit.`
+    )
+  })
+
+  it('should throw for leading space', () => {
+    expect(() => unit('  22px')).toThrow(
+      `The value '  22px' is not a valid unit.`
+    )
+  })
+
+  it('should throw for trailing space', () => {
+    expect(() => unit('22px  ')).toThrow(
+      `The value '22px  ' is not a valid unit.`
+    )
+  })
+
+  it('should throw for leading and trailing space', () => {
+    expect(() => unit('  22px  ')).toThrow(
+      `The value '  22px  ' is not a valid unit.`
+    )
   })
 })
