bbf425ccb1eb198932fae0485593ce70c950aea0
devdigital <devdigital.cloud@gmail.com>
2020-06-21 12:35:41 +0100

    fix: add unit to unitless breakpoints (#15)

--- packages/ui/src/breakpoints.js
+++ packages/ui/src/breakpoints.js
@@ -1,18 +1,35 @@
+import { isNil, isObject } from '@utilz/types'
 import { useMedia } from 'react-use'
 import { unit } from './unit'
 
 // Allow indexing or property name to be used
 // e.g. breakpoints[0] or breakpoints.phone
 export const createBreakpoints = sizes => {
+  if (isNil(sizes)) {
+    throw new Error(`No breakpoints defined.`)
+  }
+
+  if (!isObject(sizes)) {
+    throw new Error(`Unexpected non object breakpoints.`)
+  }
+
   const sizeKeys = Object.keys(sizes)
-  const bps = sizeKeys.map(s => unit(sizes[s]).css())
+  const breakpoints = sizeKeys.map(s => unit(sizes[s]).css())
+
+  sizeKeys.forEach(key => {
+    breakpoints[key] = unit(sizes[key]).css()
+  })
 
   const min = size => {
+    if (isNil(size)) {
+      throw new Error(`No breakpoint size specified.`)
+    }
+
     if (!sizeKeys.includes(size)) {
       throw new Error(`No breakpoint size '${size}' found.`)
     }
 
-    return `(min-width: ${sizes[size]})`
+    return `(min-width: ${breakpoints[size]})`
   }
 
   const useMinMediaQuery = size => {
@@ -23,15 +40,7 @@ export const createBreakpoints = sizes => {
     mediaQuery: {
       min: size => `@media ${min(size)}`,
     },
-    breakpoints: new Proxy(bps, {
-      get: (target, name) => {
-        if (Object.keys(sizes).includes(name)) {
-          return sizes[name]
-        }
-
-        return target[name]
-      },
-    }),
+    breakpoints,
     useMediaQuery: () => ({
       min: useMinMediaQuery,
     }),
