4cf5e653275f6ab957ae2bc27a04de6de2b8aace
devdigital <devdigital.cloud@gmail.com>
2020-12-21 16:38:06 +0000

    chore: add cra

--- packages/jest.config.js
+++ packages/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   rootDir: './src',
-  //   setupFilesAfterEnv: ['jest-extended', '<rootDir>/test/setup-tests.ts'],
   moduleNameMapper: {
     '^test/(.*)$': '<rootDir>/test/$1',
   },
