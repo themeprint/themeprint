061ca30fea9924c1b24e5cbfe37ea115a5792203
devdigital <devdigital.cloud@gmail.com>
2021-06-12 15:59:20 +0100

    chore: update packages (#34)

--- packages/ui/src/theme.test.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from './theme'
-import { lint } from '@themeprint/lint'
-
-describe('createTheme', () => {
-  it('should be recommended theme', () => {
-    expect(lint(createTheme()).isRecommended).toEqual(true)
-  })
-})
