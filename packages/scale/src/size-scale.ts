48014b1d1eabc82e6f980af6726b79618c7490a6
devdigital <devdigital.cloud@gmail.com>
2022-02-06 17:22:42 +0000

    feat: add scale and unit packages

---
+++ packages/scale/src/size-scale.ts
@@ -0,0 +1,173 @@
+import { isNil, isFunction, Nullish } from '@utilz/types'
+import { deepmerge } from '@utilz/deepmerge'
+import { unit } from '@themeprint/unit'
+
+// TODO: type
+const unfold = (fn: any, seed: any) => {
+  let pair = fn(seed)
+  const result = []
+
+  while (pair && pair.length) {
+    result[result.length] = pair[0]
+    pair = fn(pair[1])
+  }
+
+  return result
+}
+
+const fromEntries = (arr: any) =>
+  Object.assign({}, ...Array.from(arr, ([k, v]) => ({ [k]: v })))
+
+export interface GeneratorParams {
+  type: string
+  name: string
+  medium: number | string
+  previous: number | Nullish<string>
+}
+
+export interface ConfigureSizeScaleOptions {
+  round?: boolean
+}
+
+export const configureSizeScale =
+  (options: ConfigureSizeScaleOptions = {}) =>
+  (generator: (params: GeneratorParams) => {}) =>
+  (m: number | string) => {
+    const defaultOptions: ConfigureSizeScaleOptions = {
+      round: false,
+    }
+
+    const { round } = deepmerge<ConfigureSizeScaleOptions>(
+      defaultOptions,
+      options
+    )
+
+    if (isNil(generator)) {
+      throw new Error(`No generator specified.`)
+    }
+
+    if (!isFunction(generator)) {
+      throw new Error(`Generator must be a function.`)
+    }
+
+    const s = unfold(
+      (v: any) =>
+        v.name === 'xxxxs'
+          ? false
+          : [
+              [v.name, v.value],
+              {
+                name: `x${v.name}`,
+                value: generator({
+                  type: 's',
+                  name: `x${v.name}`,
+                  medium: m,
+                  previous: v.value,
+                }),
+              },
+            ],
+      {
+        name: 's',
+        value: generator({
+          type: 's',
+          name: 's',
+          medium: m,
+          previous: m,
+        }),
+      }
+    )
+
+    const l = unfold(
+      (v: any) =>
+        v.name === 'xxxxl'
+          ? false
+          : [
+              [v.name, v.value],
+              {
+                name: `x${v.name}`,
+                value: generator({
+                  type: 'l',
+                  name: `x${v.name}`,
+                  medium: m,
+                  previous: v.value,
+                }),
+              },
+            ],
+      {
+        name: 'l',
+        value: generator({
+          type: 'l',
+          name: 'l',
+          medium: m,
+          previous: m,
+        }),
+      }
+    )
+
+    const scale = fromEntries(
+      s
+        .concat([
+          [
+            'm',
+            generator({
+              type: 'm',
+              name: 'm',
+              medium: m,
+              previous: undefined,
+            }),
+          ],
+        ])
+        .concat(l)
+    )
+
+    if (round) {
+      return Object.keys(scale).reduce((result: Record<string, unknown>, k) => {
+        const u = unit(scale[k])
+        result[k] = u.unitless
+          ? Math.round(u.value)
+          : `${Math.round(u.value)}${u.unit}`
+        return result
+      }, {})
+    }
+
+    return scale
+  }
+
+export const sizeScale = configureSizeScale()
+
+export const ratio = (small: number, large?: number) => {
+  if (isNil(small)) {
+    throw new Error('No ratio specified.')
+  }
+
+  const s = small
+  const l = large ? large : small
+
+  return ({ type, medium, previous }: GeneratorParams): number | string => {
+    switch (type) {
+      case 's':
+        if (!previous) {
+          throw new Error(
+            'Unexpected missing previous value calculating smaller unit.'
+          )
+        }
+
+        const sp = unit(previous)
+        const su = unit(`${sp.value * (1 / s)}${sp.unit}`)
+        return sp.unitless ? su.value : su.css()
+      case 'l':
+        if (!previous) {
+          throw new Error(
+            'Unexpected missing previous value calculating larger unit.'
+          )
+        }
+
+        const lp = unit(previous)
+        const lu = unit(`${lp.value * l}${lp.unit}`)
+        return lp.unitless ? lu.value : lu.css()
+      default:
+        const m = unit(medium)
+        return m.unitless ? m.value : m.css()
+    }
+  }
+}
---
+++ packages/unit/src/unit.ts
@@ -0,0 +1,92 @@
+import { toNumeric, isString, isNil, Nullish } from '@utilz/types'
+
+const isValidUnit = (value?: Nullish<string>) => {
+  if (!value) {
+    return false
+  }
+
+  if (!isString(value)) {
+    return false
+  }
+
+  const validUnits = [
+    'cm',
+    'mm',
+    'in',
+    'px',
+    'pt',
+    'pc',
+    'em',
+    'ex',
+    'ch',
+    'rem',
+    'vw',
+    'vh',
+    'vmin',
+    'vmax',
+    '%',
+  ]
+
+  return validUnits.includes(value)
+}
+
+export interface Unit {
+  value: number
+  unit: string
+  unitless: boolean
+  css: () => string
+}
+
+function isStringGuard(value?: Nullish<number | string>): value is string {
+  return isString(value)
+}
+
+export const unit = (value?: Nullish<number | string>): Unit => {
+  if (isNil(value)) {
+    throw new Error('No value specified.')
+  }
+
+  const invalid = (): never => {
+    throw new Error(
+      `The value '${
+        isString(value) ? value : JSON.stringify(value)
+      }' is not a valid unit.`
+    )
+  }
+
+  const number = toNumeric(value)
+
+  if (number.isValid) {
+    return {
+      value: number.value,
+      unit: 'px',
+      unitless: true,
+      css: () => `${number.value}px`,
+    }
+  }
+
+  if (isStringGuard(value)) {
+    const numParts = value.split(/([\d|.]+)/).filter(Boolean)
+    if (!numParts || numParts.length !== 2) {
+      return invalid()
+    }
+
+    const number = toNumeric(numParts[0])
+    if (!number.isValid || number.value == null) {
+      return invalid()
+    }
+
+    if (!isValidUnit(numParts[1])) {
+      return invalid()
+    }
+
+    return {
+      value: number.value,
+      unit: numParts[1],
+      unitless: false,
+      css: () => `${number.value}${numParts[1]}`,
+    }
+  }
+
+  return invalid()
+}
